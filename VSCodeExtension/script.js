028e376ceac0b3843cc0b403bedd6a315f7a9d64
Michael Valenzuela <MVale-S727@chusd.org>
2025-11-10 11:42:18 -0800

    Initial commit

---
+++ VSCodeExtension/script.js
@@ -0,0 +1,7 @@
+// This is a Javascript Comment
+// ? This is a question Javascript Comment
+// ! This is an alert Javascript Comment
+// todo This is a todo Javascript Comment
+// * This is another regular Javascript Comment 
+
+window.alert("Hi! I'm Julieta Duran Vasquez!!!1!1!11!!");
